87ddbcb87fbc5fc564d600c7dbfbc7ed71136f15
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-07-15 16:15:18 +0900

    add comment

--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 if(filter_var($_GET['url'], FILTER_VALIDATE_URL)) {
     header("location: ".$_GET['url']);
 }
+
+echo 'この文が見えていれば<a href="https://bugs.php.net/bug.php?id=81122">脆弱性(リダイレクト)</a>を回避できている。';
